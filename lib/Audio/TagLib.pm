3d36b53081e1dd663f8bed722b8b38646bcba9c2
Geoffrey Leach <geoff@hughes.net>
2012-08-05 22:56:48 +0000

    Import of GLEACH/Audio-TagLib-1.56 from CPAN.
    gitpan-cpan-distribution: Audio-TagLib
    gitpan-cpan-version:      1.56
    gitpan-cpan-path:         GLEACH/Audio-TagLib-1.56.tgz
    gitpan-cpan-author:       GLEACH
    gitpan-cpan-maturity:     released

--- lib/Audio/TagLib.pm
+++ lib/Audio/TagLib.pm
@@ -4,7 +4,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '1.55';
+our $VERSION = '1.56';
 
 use XSLoader;
 XSLoader::load( 'Audio::TagLib', $VERSION );
@@ -126,7 +126,7 @@ between tag and file formats can all be ignored.
 
 =head1 VERSION
 
-1.55
+1.56
 
 =head1 DESCRIPTION
 
--- lib/Audio/TagLib/ID3v2/RelativeVolumeFrame.pm
+++ lib/Audio/TagLib/ID3v2/RelativeVolumeFrame.pm
@@ -4,7 +4,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '1.55';
+our $VERSION = '1.56';
 
 use Audio::TagLib;
 
